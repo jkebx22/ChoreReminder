57cb12a3a7d6069a0283ccc7bd04da834ba1c4fd
jkebx22 <42923140+jkebx22@users.noreply.github.com>
2019-01-30 18:44:22 -0600

    Add files via upload

---
+++ DailyChore/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace DailyChore
+{
+    partial class DailyChore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.laundry_Box = new System.Windows.Forms.TextBox();
+            this.laundryYesButton = new System.Windows.Forms.Button();
+            this.laundryNoButton = new System.Windows.Forms.Button();
+            this.dish_Box = new System.Windows.Forms.TextBox();
+            this.dishesYesButton = new System.Windows.Forms.Button();
+            this.clothes_Box = new System.Windows.Forms.TextBox();
+            this.clothesNoButton = new System.Windows.Forms.Button();
+            this.clothesYesButton = new System.Windows.Forms.Button();
+            this.dinner_Box = new System.Windows.Forms.TextBox();
+            this.dinnerYesButton = new System.Windows.Forms.Button();
+            this.dinnerNoButton = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.dishesNoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // laundry_Box
+            // 
+            this.laundry_Box.Location = new System.Drawing.Point(12, 12);
+            this.laundry_Box.Name = "laundry_Box";
+            this.laundry_Box.Size = new System.Drawing.Size(277, 22);
+            this.laundry_Box.TabIndex = 0;
+            this.laundry_Box.Text = "Did you do the laundry?";
+            // 
+            // laundryYesButton
+            // 
+            this.laundryYesButton.Location = new System.Drawing.Point(13, 41);
+            this.laundryYesButton.Name = "laundryYesButton";
+            this.laundryYesButton.Size = new System.Drawing.Size(75, 57);
+            this.laundryYesButton.TabIndex = 1;
+            this.laundryYesButton.Text = "Yes!";
+            this.laundryYesButton.UseVisualStyleBackColor = true;
+            this.laundryYesButton.Click += new System.EventHandler(this.laundryYesButton_Click);
+            // 
+            // laundryNoButton
+            // 
+            this.laundryNoButton.Location = new System.Drawing.Point(214, 41);
+            this.laundryNoButton.Name = "laundryNoButton";
+            this.laundryNoButton.Size = new System.Drawing.Size(75, 57);
+            this.laundryNoButton.TabIndex = 2;
+            this.laundryNoButton.Text = "No";
+            this.laundryNoButton.UseVisualStyleBackColor = true;
+            this.laundryNoButton.Click += new System.EventHandler(this.laundryNoButton_Click);
+            // 
+            // dish_Box
+            // 
+            this.dish_Box.Location = new System.Drawing.Point(13, 121);
+            this.dish_Box.Name = "dish_Box";
+            this.dish_Box.Size = new System.Drawing.Size(276, 22);
+            this.dish_Box.TabIndex = 3;
+            this.dish_Box.Text = "Did you do the dishes?";
+            // 
+            // dishesYesButton
+            // 
+            this.dishesYesButton.Location = new System.Drawing.Point(12, 149);
+            this.dishesYesButton.Name = "dishesYesButton";
+            this.dishesYesButton.Size = new System.Drawing.Size(75, 57);
+            this.dishesYesButton.TabIndex = 4;
+            this.dishesYesButton.Text = "Yes!";
+            this.dishesYesButton.UseVisualStyleBackColor = true;
+            this.dishesYesButton.Click += new System.EventHandler(this.dishesYesButton_Click);
+            // 
+            // clothes_Box
+            // 
+            this.clothes_Box.Location = new System.Drawing.Point(12, 231);
+            this.clothes_Box.Name = "clothes_Box";
+            this.clothes_Box.Size = new System.Drawing.Size(277, 22);
+            this.clothes_Box.TabIndex = 6;
+            this.clothes_Box.Text = "Did you put up your clothes?";
+            // 
+            // clothesNoButton
+            // 
+            this.clothesNoButton.Location = new System.Drawing.Point(214, 259);
+            this.clothesNoButton.Name = "clothesNoButton";
+            this.clothesNoButton.Size = new System.Drawing.Size(75, 57);
+            this.clothesNoButton.TabIndex = 7;
+            this.clothesNoButton.Text = "No";
+            this.clothesNoButton.UseVisualStyleBackColor = true;
+            this.clothesNoButton.Click += new System.EventHandler(this.clothesNoButton_Click);
+            // 
+            // clothesYesButton
+            // 
+            this.clothesYesButton.Location = new System.Drawing.Point(12, 259);
+            this.clothesYesButton.Name = "clothesYesButton";
+            this.clothesYesButton.Size = new System.Drawing.Size(75, 57);
+            this.clothesYesButton.TabIndex = 8;
+            this.clothesYesButton.Text = "Yes!";
+            this.clothesYesButton.UseVisualStyleBackColor = true;
+            this.clothesYesButton.Click += new System.EventHandler(this.clothesYesButton_Click);
+            // 
+            // dinner_Box
+            // 
+            this.dinner_Box.Location = new System.Drawing.Point(12, 342);
+            this.dinner_Box.Name = "dinner_Box";
+            this.dinner_Box.Size = new System.Drawing.Size(276, 22);
+            this.dinner_Box.TabIndex = 9;
+            this.dinner_Box.Text = "Did you make dinner?";
+            // 
+            // dinnerYesButton
+            // 
+            this.dinnerYesButton.Location = new System.Drawing.Point(12, 370);
+            this.dinnerYesButton.Name = "dinnerYesButton";
+            this.dinnerYesButton.Size = new System.Drawing.Size(75, 57);
+            this.dinnerYesButton.TabIndex = 10;
+            this.dinnerYesButton.Text = "Yes!";
+            this.dinnerYesButton.UseVisualStyleBackColor = true;
+            this.dinnerYesButton.Click += new System.EventHandler(this.dinnerYesButton_Click);
+            // 
+            // dinnerNoButton
+            // 
+            this.dinnerNoButton.Location = new System.Drawing.Point(213, 370);
+            this.dinnerNoButton.Name = "dinnerNoButton";
+            this.dinnerNoButton.Size = new System.Drawing.Size(75, 57);
+            this.dinnerNoButton.TabIndex = 11;
+            this.dinnerNoButton.Text = "No";
+            this.dinnerNoButton.UseVisualStyleBackColor = true;
+            this.dinnerNoButton.Click += new System.EventHandler(this.dinnerNoButton_Click);
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Location = new System.Drawing.Point(12, 458);
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(276, 22);
+            this.textBox_Result.TabIndex = 12;
+            // 
+            // dishesNoButton
+            // 
+            this.dishesNoButton.Location = new System.Drawing.Point(213, 149);
+            this.dishesNoButton.Name = "dishesNoButton";
+            this.dishesNoButton.Size = new System.Drawing.Size(75, 57);
+            this.dishesNoButton.TabIndex = 13;
+            this.dishesNoButton.Text = "No";
+            this.dishesNoButton.UseVisualStyleBackColor = true;
+            this.dishesNoButton.Click += new System.EventHandler(this.dishesNoButton_Click);
+            // 
+            // DailyChore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 503);
+            this.Controls.Add(this.dishesNoButton);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.dinnerNoButton);
+            this.Controls.Add(this.dinnerYesButton);
+            this.Controls.Add(this.dinner_Box);
+            this.Controls.Add(this.clothesYesButton);
+            this.Controls.Add(this.clothesNoButton);
+            this.Controls.Add(this.clothes_Box);
+            this.Controls.Add(this.dishesYesButton);
+            this.Controls.Add(this.dish_Box);
+            this.Controls.Add(this.laundryNoButton);
+            this.Controls.Add(this.laundryYesButton);
+            this.Controls.Add(this.laundry_Box);
+            this.Name = "DailyChore";
+            this.Text = "DailyChore";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox laundry_Box;
+        private System.Windows.Forms.Button laundryYesButton;
+        private System.Windows.Forms.Button laundryNoButton;
+        private System.Windows.Forms.TextBox dish_Box;
+        private System.Windows.Forms.Button dishesYesButton;
+        private System.Windows.Forms.Button dishesNoButton;
+        private System.Windows.Forms.TextBox clothes_Box;
+        private System.Windows.Forms.Button clothesNoButton;
+        private System.Windows.Forms.Button clothesYesButton;
+        private System.Windows.Forms.TextBox dinner_Box;
+        private System.Windows.Forms.Button dinnerYesButton;
+        private System.Windows.Forms.Button dinnerNoButton;
+        private System.Windows.Forms.TextBox textBox_Result;
+    }
+}
+
---
+++ DailyChore/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyChore
+{
+    public partial class DailyChore : Form
+    {
+
+        public DailyChore()
+        {
+            InitializeComponent();
+        }
+
+        private void laundryYesButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Great job on the laundry!";
+
+        }
+
+        private void laundryNoButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Quit being lazy on the laundry!";
+        }
+
+        private void dishesYesButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Great job on the dishes!";
+        }
+
+        private void dishesNoButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Quit being lazy on the dishes!";
+        }
+
+        private void clothesYesButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Great job on the clothes!";
+        }
+
+        private void clothesNoButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Quit being lazy on the clothes!";
+        }
+
+        private void dinnerYesButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Great job on cooking dinner!";
+        }
+
+        private void dinnerNoButton_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = " Quit being lazy on the clothes!";
+        }
+    }
+}
